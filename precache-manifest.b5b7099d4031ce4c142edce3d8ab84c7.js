e45d1a09c9c49ce34547520b8e34d8ff5c53eba6
amitsarna <amit.sarna.584@gmail.com>
2020-04-19 22:46:22 +0100

    Updates

---
+++ precache-manifest.b5b7099d4031ce4c142edce3d8ab84c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4393b3c9eddcf44bd80377747a3d190",
+    "url": "/ReadWordsKyan/index.html"
+  },
+  {
+    "revision": "a9913bdda3863455481a",
+    "url": "/ReadWordsKyan/static/css/main.df5add57.chunk.css"
+  },
+  {
+    "revision": "3efc7ddb39cc7302054b",
+    "url": "/ReadWordsKyan/static/js/2.bc66cd20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReadWordsKyan/static/js/2.bc66cd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9913bdda3863455481a",
+    "url": "/ReadWordsKyan/static/js/main.8fe81659.chunk.js"
+  },
+  {
+    "revision": "3eece471ae6c8b7aa23e",
+    "url": "/ReadWordsKyan/static/js/runtime-main.ea5b41f1.js"
+  }
+]);
